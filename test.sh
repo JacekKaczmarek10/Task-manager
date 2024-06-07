8ee5c7b8e6deca652ab57114e18156f786e7ff7a
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-06-07 16:42:22 +0200

    fix: update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-go test ./controllers/product-controller.go  ./controllers/product-controller_test.go
+go test ./product/product-controller.go ./product/product.go ./product/productrepo.go  ./product/product-controller_test.go
