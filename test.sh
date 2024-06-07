76bb3e150878ab2ddb698c0d1734fc55ee12ed89
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-06-07 16:47:25 +0200

    feat: update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
 go test ./product/product-controller.go ./product/product.go ./product/productrepo.go  ./product/product-controller_test.go
+go test ./product/product.go ./product/productrepo.go  ./product/productrepo_test.go
